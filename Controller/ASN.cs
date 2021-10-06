ee105f4311575d21a49df818bda21293a51232d6
RavenGovender <govender.ravendran@gmail.com>
2021-10-06 16:42:39 +0200

    intial deploy test

---
+++ Controller/ASN.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class ASN
+{
+	public int asn_num { get; set; }
+	public String reg_date { get; set; }
+	public String country { get; set; }
+	public String region { get; set; }
+	public int reg_year { get; set; }
+	public String industry { get; set; }
+	public String org_category { get; set; }
+	public String asn_type { get; set; }
+}
